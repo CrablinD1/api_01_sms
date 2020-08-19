e49750bb28c04776d32925b637cc91b04c3df396
CrablinD1 <crablin@mail.com>
2020-08-19 20:10:27 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
-url = os.getenv('URL')
 ACCOUNT_SID = os.getenv('ACCOUNT_SID')
 AUTH_TOKEN = os.getenv('AUTH_TOKEN')
 NUMBER_FROM = os.getenv('NUMBER_FROM')
@@ -21,7 +20,8 @@ def get_status(user_id):
         'access_token': ACCESS_TOKEN,
         'v': '5.92'
     }
-    result = requests.post(url, params=params).json().get('response')
+    result = requests.post('https://api.vk.com/method/users.get',
+                           params=params).json().get('response')
     status = result[0]['online']
     return status
 

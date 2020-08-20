35872e3f9f2a95438f73630614d139c2e514fb63
CrablinD1 <64089854+CrablinD1@users.noreply.github.com>
2020-08-20 10:51:29 +0300

    Update homework.py
    fix and logging

--- homework.py
+++ homework.py
@@ -1,16 +1,20 @@
-import time
-import requests
+import logging
 import os
-from twilio.rest import Client
+import time
+
+import requests
 from dotenv import load_dotenv
+from twilio.rest import Client
 
 load_dotenv()
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 ACCOUNT_SID = os.getenv('ACCOUNT_SID')
 AUTH_TOKEN = os.getenv('AUTH_TOKEN')
+VERSION = os.getenv('VERSION')
 NUMBER_FROM = os.getenv('NUMBER_FROM')
 NUMBER_TO = os.getenv('NUMBER_TO')
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
+url = 'https://api.vk.com/method/{}'
 
 
 def get_status(user_id):
@@ -18,11 +22,15 @@ def get_status(user_id):
         'user_ids': user_id,
         'fields': 'online',
         'access_token': ACCESS_TOKEN,
-        'v': '5.92'
+        'v': VERSION
     }
-    result = requests.post('https://api.vk.com/method/users.get',
-                           params=params).json().get('response')
-    status = result[0]['online']
+    try:
+        result = requests.post(url.format('users.get'),
+                               params=params).json().get('response')
+        status = result[0]['online']
+    except requests.exceptions.RequestException as error:
+        logging.error(f'Произошла ошибка: {error}')
+        return None
     return status
 
 
@@ -41,6 +49,5 @@ if __name__ == "__main__":
     while True:
         if get_status(vk_id) == 1:
             sms_sender(f'{vk_id} сейчас онлайн!')
-            print(f'{vk_id} сейчас онлайн!')
             break
         time.sleep(5)
